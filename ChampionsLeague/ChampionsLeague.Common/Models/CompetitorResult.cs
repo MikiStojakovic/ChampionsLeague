477bc655326aee75279fb3ec6b721100ad52de89
Miroslav Stojakovic <miroslav.miki.stojakovic@gmail.com>
2018-08-09 19:27:43 +0200

    Added new model classes Added IConvertable interface

---
+++ ChampionsLeague/ChampionsLeague.Common/Models/CompetitorResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChampionsLeague.Common.Models
+{
+    public class CompetitorResult
+    {
+        public string Team { get; set; }
+        public int Wins { get; set; }
+        public int Lost{ get; set; }
+    }
+}
---
+++ ChampionsLeague/ChampionsLeague.Common/Models/TableOrder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChampionsLeague.Common.Models
+{
+    public class TableOrder
+    {
+        public IList<CompetitorResult> TableRowCollection { get; set; }
+    }
+}
